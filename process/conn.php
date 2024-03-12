d42c4501e829f6910a6f5f2bfd1307482f341634
vicento-cares <143.careappsoftware@gmail.com>
2024-03-12 17:19:01 +0800

    Version 1.0.3

--- process/conn.php
+++ process/conn.php
@@ -1,8 +1,8 @@
 <?php
 include 'server_date_time.php';
 
-$servername = 'localhost'; $username = 'root'; $password = '';
-// $servername = '172.25.116.188'; $username = 'server_113.4'; $password = 'SystemGroup@2022';
+// $servername = 'localhost'; $username = 'root'; $password = '';
+$servername = '172.25.116.188'; $username = 'server_113.4'; $password = 'SystemGroup@2022';
 
 try {
     $conn = new PDO ("mysql:host=$servername;dbname=emp_mgt_db",$username,$password);
