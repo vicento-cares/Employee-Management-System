cfa7a79bd4d1c9b94054d27d4e9fa227a7b97767
vicento-cares <143.careappsoftware@gmail.com>
2024-03-13 12:00:23 +0800

    Version 1.0.3

--- admin/plugins/js/home_script.php
+++ admin/plugins/js/home_script.php
@@ -1,14 +1,14 @@
 <script type="text/javascript">
 	// DOMContentLoaded function
 	document.addEventListener("DOMContentLoaded", () => {
-		get_attendance_date_ds();
-		get_attendance_date_ns();
+		get_attendance_date();
 		get_recent_time_in_out_ds();
 		get_recent_time_in_out_ns();
-		setInterval(get_attendance_date_ds, 10000);
-		setInterval(get_attendance_date_ns, 10000);
+		get_recent_time_in_out_ads();
+		setInterval(get_attendance_date, 10000);
 		setInterval(get_recent_time_in_out_ds, 10000);
 		setInterval(get_recent_time_in_out_ns, 10000);
+		setInterval(get_recent_time_in_out_ads, 10000);
 		sessionStorage.setItem('notif_pending_ls', 0);
 		sessionStorage.setItem('notif_accepted_ls', 0);
 		sessionStorage.setItem('notif_rejected_ls', 0);
@@ -16,41 +16,35 @@
 		realtime_load_notif_line_support = setInterval(load_notif_line_support, 5000);
 	});
 
-	const get_attendance_date_ds = () => {
+	const get_attendance_date = () => {
 		$.ajax({
-			type: "POST",
+			type: "GET",
 			url: "../process/admin/time_in_out/tio_p.php",
 			cache: false,
 			data: {
-				method: "get_attendance_date_ds"
+				method: "get_attendance_date"
 			},
 			success: (response) => {
-				$('#day_view_ds').html(`Attendance Date: ${response}`);
-			}
-		});
-	}
-
-	const get_attendance_date_ns = () => {
-		$.ajax({
-			type: "POST",
-			url: "../process/admin/time_in_out/tio_p.php",
-			cache: false,
-			data: {
-				method: "get_attendance_date_ns"
-			},
-			success: (response) => {
-				$('#day_view_ns').html(`Attendance Date: ${response}`);
+				try {
+                    let response_array = JSON.parse(response);
+					$('#day_view_ds').html(`Attendance Date: ${response_array.day_view_ds}`);
+					$('#day_view_ns').html(`Attendance Date: ${response_array.day_view_ns}`);
+					$('#day_view_ads').html(`Attendance Date: ${response_array.day_view_ads}`);
+                } catch (e) {
+                    console.log(response);
+                }
 			}
 		});
 	}
 
 	const get_recent_time_in_out_ds = () => {
 		$.ajax({
-			type: "POST",
+			type: "GET",
 			url: "../process/admin/time_in_out/tio_p.php",
 			cache: false,
 			data: {
-				method: "get_recent_time_in_out_ds"
+				method: "get_recent_time_in_out",
+				shift_group: 'A'
 			},
 			success: (response) => {
 				$('#recentTimeInOutDsData').html(response);
@@ -62,16 +56,34 @@
 
 	const get_recent_time_in_out_ns = () => {
 		$.ajax({
-			type: "POST",
+			type: "GET",
 			url: "../process/admin/time_in_out/tio_p.php",
 			cache: false,
 			data: {
-				method: "get_recent_time_in_out_ns"
+				method: "get_recent_time_in_out",
+				shift_group: 'B'
 			},
 			success: (response) => {
 				$('#recentTimeInOutNsData').html(response);
 				let table_rows = parseInt(document.getElementById("recentTimeInOutNsData").childNodes.length);
 				$('#count_view_ns').html("Total MP: " + table_rows);
+			}
+		});
+	}
+
+	const get_recent_time_in_out_ads = () => {
+		$.ajax({
+			type: "GET",
+			url: "../process/admin/time_in_out/tio_p.php",
+			cache: false,
+			data: {
+				method: "get_recent_time_in_out",
+				shift_group: 'ADS'
+			},
+			success: (response) => {
+				$('#recentTimeInOutAdsData').html(response);
+				let table_rows = parseInt(document.getElementById("recentTimeInOutAdsData").childNodes.length);
+				$('#count_view_ads').html("Total MP: " + table_rows);
 			}
 		});
 	}
