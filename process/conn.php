a1e6d2ab276a7299e72705a94e565778a6f9f235
vicento-cares <143.careappsoftware@gmail.com>
2024-06-05 09:10:48 +0800

    MS SQL Server Connection Update

--- process/conn.php
+++ process/conn.php
@@ -1,11 +1,16 @@
 <?php
 include 'server_date_time.php';
 
+// Old MySql Connection
 $servername = 'localhost'; $username = 'root'; $password = '';
 // $servername = '172.25.116.188'; $username = 'server_113.4'; $password = 'SystemGroup@2022';
 
+// MS SQL Server Connection
+// $servername = '172.25.116.188\SQLEXPRESS'; $username = 'SA'; $password = 'SystemGroup2018';
+
 try {
     $conn = new PDO ("mysql:host=$servername;dbname=emp_mgt_db",$username,$password);
+    // $conn = new PDO ("sqlsrv:Server=$servername;Database=emp_mgt_db",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'NO CONNECTION'.$e->getMessage();
