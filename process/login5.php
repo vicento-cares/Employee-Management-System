9b1ffe22a43f6fa719de9fdb4587d08a0758eff4
vicento-cares <143.careappsoftware@gmail.com>
2025-01-06 13:01:59 +0800

    Control Area Account Update

--- process/login5.php
+++ process/login5.php
@@ -15,18 +15,29 @@ if (isset($_POST['login_btn'])) {
         // MySQL
         // $check = "SELECT emp_no, full_name, dept, section, line_no, shift_group, role FROM m_control_area_accounts WHERE BINARY emp_no = '$emp_no'";
         // MS SQL Server
-        $check = "SELECT emp_no, full_name, dept, section, line_no, shift_group, role FROM m_control_area_accounts WHERE emp_no = '$emp_no' COLLATE SQL_Latin1_General_CP1_CS_AS";
-        $stmt = $conn->prepare($check, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+        $check = "
+            SELECT emp_no, full_name, dept, section, line_no, shift_group, role 
+            FROM m_control_area_accounts 
+            WHERE emp_no = '$emp_no' COLLATE SQL_Latin1_General_CP1_CS_AS
+            UNION
+            SELECT emp_no, full_name, dept, section, line_no, shift_group, role 
+            FROM m_accounts 
+            WHERE emp_no = '$emp_no' COLLATE SQL_Latin1_General_CP1_CS_AS
+        ";
+        $stmt = $conn->prepare($check);
         $stmt->execute();
-        if ($stmt->rowCount() > 0) {
-            foreach($stmt->fetchALL() as $x){
-                $emp_no = $x['emp_no'];
-                $full_name = $x['full_name'];
-                $dept = $x['dept'];
-                $section = $x['section'];
-                $line_no = $x['line_no'];
-                $shift_group = $x['shift_group'];
-                $role = $x['role'];
+        
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if (count($results) > 0) {
+			foreach ($results as $row) {
+                $emp_no = $row['emp_no'];
+                $full_name = $row['full_name'];
+                $dept = $row['dept'];
+                $section = $row['section'];
+                $line_no = $row['line_no'];
+                $shift_group = $row['shift_group'];
+                $role = $row['role'];
             }
             $_SESSION['emp_no_control_area'] = $emp_no;
             $_SESSION['full_name'] = $full_name;
@@ -51,4 +62,3 @@ if (isset($_POST['Logout'])) {
     session_destroy();
     header('location:/emp_mgt/control_area');
 }
-?>
