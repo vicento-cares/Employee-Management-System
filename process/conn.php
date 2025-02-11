bb34a95cc463001b2b5561d549a419e7f16ab9a6
vicento-falp <vince.dale.alcantara@furukawaelectric.com>
2025-02-11 15:43:58 +0800

    Update conn.php
    test publish

--- process/conn.php
+++ process/conn.php
@@ -6,7 +6,7 @@ include 'server_date_time.php';
 // $servername = '172.25.116.188'; $username = 'server_113.4'; $password = 'SystemGroup@2022';
 
 // MS SQL Server Connection
-// $servername = '172.25.112.131, 1433\SQLEXPRESS'; $username = 'SA'; $password = 'SystemGroup2018';
+// $servername = '172.25.112.131, 1433\SQLEXPRESS'; $username = 'SA'; $password = 'SystemGroup2018';a
 $servername = '172.25.116.188'; $username = 'SA'; $password = 'SystemGroup@2022';
 
 try {
