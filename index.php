95326197f43cc32a553f111fdab0a98613459272
vicento-cares <143.careappsoftware@gmail.com>
2025-02-11 08:56:54 +0800

    Version Label Update

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
         <footer class="main-footer">
             <!-- To the right -->
             <div class="float-right d-none d-sm-inline">
-                <b>Beta Version</b> 1.1.1
+                <b>Beta Version</b> 1.1.2
             </div>
             <!-- Default to the left -->
             <strong>Copyright &copy; 2023 Vince Dale Alcantara.</strong>
