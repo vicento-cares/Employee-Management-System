b8bc84b8434de2e750c7ed1ad5023f54078a0028
vicento-cares <143.careappsoftware@gmail.com>
2025-11-03 11:26:09 +0800

    v1.1.10 - HR Accounts

--- hr/index.php
+++ hr/index.php
@@ -35,13 +35,21 @@ if (isset($_SESSION['emp_no_hr'])) {
 
         <form action="" method="POST" id="login_form">
           <div class="input-group mb-3">
-            <input type="password" class="form-control" id="emp_no" name="emp_no" placeholder="ID Number" autocomplete="off" required>
+            <input type="text" class="form-control" id="emp_no" name="emp_no" placeholder="ID Number" autocomplete="off" required>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password" autocomplete="off" required>
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
               </div>
             </div>
-          </div>   
+          </div>
           <!-- /.col -->
           <div class="input-group mb-3">
             <button type="submit" class="btn btn-primary btn-block" name="login_btn" value="login">Sign In</button>
--- process/login4.php
+++ process/login4.php
@@ -7,19 +7,25 @@ include 'conn.php';
 
 if (isset($_POST['login_btn'])) {
     $emp_no = $_POST['emp_no'];
+    $password = $_POST['password'];
 
     if (empty($emp_no)) {
         echo '<script>alert("Please Scan QR Code or Enter ID Number")</script>';
+    } else if (empty($password)) {
+        echo '<script>alert("Please Enter Password")</script>';
     } else {
         // MySQL
         // $check = "SELECT emp_no, full_name, dept, section, line_no, role FROM m_hr_accounts WHERE BINARY emp_no = '$emp_no' AND role = 'hr'";
         // MS SQL Server
         $check = "SELECT emp_no, full_name, dept, section, line_no, role 
                     FROM m_hr_accounts 
-                    WHERE emp_no = ? AND role = 'hr' COLLATE SQL_Latin1_General_CP1_CS_AS";
+                    WHERE 
+                        emp_no = ? COLLATE SQL_Latin1_General_CP1_CS_AS AND 
+                        password = ? COLLATE SQL_Latin1_General_CP1_CS_AS AND 
+                        role = 'hr' ";
         
         $stmt = $conn->prepare($check);
-        $params = array($emp_no);
+        $params = array($emp_no, $password);
         $stmt->execute($params);
         
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
