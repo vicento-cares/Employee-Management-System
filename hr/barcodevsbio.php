6c43f933c7bda658d976592e1390d811f2351710
vicento-cares <143.careappsoftware@gmail.com>
2026-02-27 08:54:17 +0800

    v1.1.12 - Biometric Vs Barcode Chart

--- hr/barcodevsbio.php
+++ hr/barcodevsbio.php
@@ -61,126 +61,106 @@
         <!-- /.col -->
       </div>
       <!-- /.row -->
-       <div class="row">
+      <div class="row">
         <div class="col-sm-12">
-          <div class="card card-gray-dark card-tabs">
-            <div class="card-header p-0 border-bottom-0">
-              <ul class="nav nav-tabs" id="bvb-tab" role="tablist">
-                <li class="nav-item">
-                  <a class="nav-link active" id="bvb-1-tab" data-toggle="pill" href="#bvb-1" role="tab" aria-controls="bvb-1" aria-selected="true">Time In Analysis</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" id="bvb-2-tab" data-toggle="pill" href="#bvb-2" role="tab" aria-controls="bvb-2" aria-selected="false">Time Out Analysis</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" id="bvb-3-tab" data-toggle="pill" href="#bvb-3" role="tab" aria-controls="bvb-3" aria-selected="false">Compliance Analysis</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" id="bvb-4-tab" data-toggle="pill" href="#bvb-4" role="tab" aria-controls="bvb-4" aria-selected="false">Barcode Vs Biometric Table Data</a>
-                </li>
-              </ul>
-            </div>
-            <div class="card-body">
-              <div class="tab-content" id="bvb-tabContent">
-                <!-- Time In Analysis -->
-                <div class="tab-pane fade show active" id="bvb-1" role="tabpanel" aria-labelledby="bvb-1-tab">
-                  <div class="row">
-                    <div class="col-12" id="month_bio_vs_barcode_time_in_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_late_time_in_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_no_bio_time_in_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_no_barcode_time_in_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_no_entries_time_in_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_early_barcode_time_in_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_late_barcode_time_in_chart"></div>
-                  </div>
-                </div>
-                <!-- Time Out Analysis -->
-                <div class="tab-pane fade" id="bvb-2" role="tabpanel" aria-labelledby="bvb-2-tab">
-                  <div class="row">
-                    <div class="col-12" id="month_bio_vs_barcode_time_out_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_no_bio_time_out_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_no_barcode_time_out_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_no_entries_time_out_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_early_bio_time_out_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_section_late_bio_time_out_chart"></div>
-                  </div>
-                </div>
-                <!-- Compliance Analysis -->
-                <div class="tab-pane fade" id="bvb-3" role="tabpanel" aria-labelledby="bvb-3-tab">
-                  <div class="row">
-                    <div class="col-12" id="month_compliance_time_in_chart"></div>
-                  </div>
-                  <div class="row">
-                    <div class="col-12" id="month_compliance_time_out_chart"></div>
-                  </div>
-                </div>
-                <!-- Barcode Vs Biometric Table Data -->
-                <div class="tab-pane fade" id="bvb-4" role="tabpanel" aria-labelledby="bvb-4-tab">
-                  <div class="row">
-                    <div class="col-sm-9">
-                      <div class="form-group mb-0">
-                        <label>No Time Out Barcode Vs Biometric Table</label>
-                      </div>
-                    </div>
-                    <div class="col-sm-3">
-                      <button type="button" class="btn bg-success btn-block" onclick="export_bio_vs_barcode_data('bioVsBarcodeTable')"><i class="fas fa-download"></i> Export Barcode Vs Biometric List</button>
-                    </div>
-                  </div>
-                  <div class="row mb-2">
-                    <div class="col-sm-2">
-                      <span id="count_view"></span>
-                    </div>
-                  </div>
-                  <div id="bioVsBarcodeTableRes" class="table-responsive" style="max-height: 500px; overflow: auto; display:inline-block;">
-                    <table id="bioVsBarcodeTable" class="table table-sm table-head-fixed table-foot-fixed text-nowrap table-hover">
-                      <thead style="text-align: center;">
-                        <tr>
-                          <th>#</th>
-                          <th>Day</th>
-                          <th>Employee No.</th>
-                          <th>Full Name</th>
-                          <th>Department</th>
-                          <th>Section</th>
-                          <th>Line No.</th>
-                          <th>Process</th>
-                          <th>Time In Remarks</th>
-                          <th>Time Out Remarks</th>
-                        </tr>
-                      </thead>
-                      <tbody id="bioVsBarcodeData" style="text-align: center;"></tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- /.card -->
+          <div class="form-group mb-0">
+            <label>Time In Analysis Charts</label>
           </div>
         </div>
-        <!-- /.col -->
       </div>
-      <!-- /.row -->
+      <div class="row">
+        <div class="col-12" id="month_bio_vs_barcode_time_in_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_late_time_in_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_no_bio_time_in_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_no_barcode_time_in_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_no_entries_time_in_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_early_barcode_time_in_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_late_barcode_time_in_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="form-group mb-0">
+            <label>Time Out Analysis Charts</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_bio_vs_barcode_time_out_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_no_bio_time_out_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_no_barcode_time_out_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_no_entries_time_out_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_early_bio_time_out_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_section_late_bio_time_out_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="form-group mb-0">
+            <label>Compliance Analysis Charts</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_compliance_time_in_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="month_compliance_time_out_chart"></div>
+      </div>
+      <div class="row">
+        <div class="col-sm-9">
+          <div class="form-group mb-0">
+            <label>No Time Out Barcode Vs Biometric Table</label>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <button type="button" class="btn bg-success btn-block" onclick="export_bio_vs_barcode_data('bioVsBarcodeTable')"><i class="fas fa-download"></i> Export Barcode Vs Biometric List</button>
+        </div>
+      </div>
+      <div class="row mb-2">
+        <div class="col-sm-2">
+          <span id="count_view"></span>
+        </div>
+      </div>
+      <div id="bioVsBarcodeTableRes" class="table-responsive" style="max-height: 500px; overflow: auto; display:inline-block;">
+        <table id="bioVsBarcodeTable" class="table table-sm table-head-fixed table-foot-fixed text-nowrap table-hover">
+          <thead style="text-align: center;">
+            <tr>
+              <th>#</th>
+              <th>Day</th>
+              <th>Employee No.</th>
+              <th>Full Name</th>
+              <th>Department</th>
+              <th>Section</th>
+              <th>Line No.</th>
+              <th>Process</th>
+              <th>Time In Remarks</th>
+              <th>Time Out Remarks</th>
+            </tr>
+          </thead>
+          <tbody id="bioVsBarcodeData" style="text-align: center;"></tbody>
+        </table>
+      </div>
     </div>
   </section>
 </div>
