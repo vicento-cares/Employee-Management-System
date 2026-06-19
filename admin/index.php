e7397cbce8b8cf4f36c469e066fc8382219f9fa2
vicento-cares <143.careappsoftware@gmail.com>
2026-06-19 13:10:19 +0800

    Prod PC Registration Admin

--- admin/index.php
+++ admin/index.php
@@ -10,6 +10,48 @@ if (isset($_SESSION['emp_no'])) {
   }
 }
 ?>
+
+<?php
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!isset($_POST['password']) || empty($_POST['password'])) {
+
+      $_SESSION['registration_alert'] = <<<HTML
+        <div class="alert alert-danger">
+          Registration Failed. No Admin Key Typed
+        </div>
+      HTML;
+
+    } else if ($_POST['password'] != 'Team@It') {
+
+      $_SESSION['registration_alert'] = <<<HTML
+        <div class="alert alert-danger">
+          Registration Failed. Wrong Admin Key
+        </div>
+      HTML;
+
+    } else {
+      //do some sketchy insert
+      $stmt = $conn -> prepare("INSERT INTO m_access_locations (dept, section, line_no, ip) VALUES (:dept, :section, :line_no, :ip)");
+      $stmt -> execute([
+        "dept" => $_POST['dept'],
+        "section" => $_POST['section'],
+        "line_no" => $_POST['line_no'],
+        "ip" => $_POST['ip_address']
+      ]);
+
+      $_SESSION['registration_alert'] = <<<HTML
+        <div class="alert alert-success">
+          Registration Successful. Attempt to Login Now
+        </div>
+      HTML;
+    }
+
+    // reset request method from post to get again with this
+    // removes the browser refresh back to a post request
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,6 +70,82 @@ if (isset($_SESSION['emp_no'])) {
 </head>
 
 <body class="hold-transition login-page">
+  <div class="position-fixed m-1 d-flex flex-column" style="top:0;right:0">
+    <div class="d-flex justify-content-end">
+      <button class="btn btn-sm btn-outline-primary" style="opacity:0;" onclick="document.getElementById('register_access_form').classList.remove('d-none');">API</button>
+    </div>
+    <?php
+      if (isset($_SESSION['registration_alert'])) {
+        echo $_SESSION['registration_alert'];
+        unset($_SESSION['registration_alert']);
+      }
+    ?>
+    <style>
+      #register_access_form .form-group {
+        margin-bottom: 0.25rem;
+      }
+    </style>
+    <form method="POST" id="register_access_form" class="d-none" autocomplete="off">
+      <div class="p-1" style="width:50vh;">
+        <div class="form-group">
+          <label for="dept" class="small">Department</label>
+          <input type="text" list="dept-list" class="form-control form-control-sm" id="dept" name="dept" required>
+          <datalist id="dept-list">
+            <?php
+              $stmt = $conn ->query("SELECT DISTINCT dept from m_access_locations");
+              while (($row = $stmt ->fetch(PDO::FETCH_COLUMN)) !== false) {
+                echo <<<HTML
+                  <option>{$row}</option>
+                HTML;
+              }
+            ?>
+          </datalist>
+          <small class="form-text text-muted small">Choose from the list set or type your own</small>
+        </div>
+        <div class="form-group">
+          <label for="section" class="small">Section</label>
+          <input type="text" list="section-list" class="form-control form-control-sm" id="section" name="section" required>
+          <datalist id="section-list">
+            <?php
+              $stmt = $conn ->query("SELECT DISTINCT section from m_access_locations");
+              while (($row = $stmt->fetch(PDO::FETCH_COLUMN)) !== false) {
+                echo <<<HTML
+                  <option>{$row}</option>
+                HTML;
+              }
+            ?>
+          </datalist>
+          <small class="form-text text-muted small">Choose from the list set or type your own</small>
+        </div>
+        <div class="form-group">
+          <label for="line_no" class="small">Line</label>
+          <input type="text" list="line-list" class="form-control form-control-sm" id="line_no" name="line_no" required>
+          <datalist id="line-list">
+            <?php
+              $stmt = $conn ->query("SELECT DISTINCT line_no from m_access_locations");
+              while (($row = $stmt->fetch(PDO::FETCH_COLUMN)) !== false) {
+                echo <<<HTML
+                  <option>{$row}</option>
+                HTML;
+              }
+            ?>
+          </datalist>
+          <small class="form-text text-muted small">Choose from the list set or type your own</small>
+        </div>
+        <div class="form-group">
+          <label for="ip_address" class="small">IP Address</label>
+          <input type="text" class="form-control form-control-sm" id="ip_address" name="ip_address" pattern="^172\.25\.\d{1,3}\.\d{1,3}$" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>" required>
+        </div>
+        <div class="form-group">
+          <label for="ip_address" class="small">Admin Key</label>
+          <input type="password" name="password" class="form-control form-control-sm" autocomplete="new-password">
+        </div>
+        <div class="d-flex justify-content-center">
+          <button class="btn btn-sm btn-success">Register Access Location</button>
+        </div>
+      </div>
+    </form>
+  </div>
   <div class="login-box">
     <div class="login-logo">
       <img src="../dist/img/logo.webp" style="height:100px;">
