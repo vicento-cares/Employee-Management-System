dcbed8139b324a61b5c718ed41c97a05f8912fd2
vicento-falp <vince.dale.alcantara@furukawaelectric.com>
2026-06-23 14:52:30 +0800

    trust certificate

--- process/conn.php
+++ process/conn.php
@@ -10,7 +10,7 @@ $servername = '172.25.116.188'; $username = 'SA'; $password = 'SystemGroup@2022'
 
 try {
     // $conn = new PDO ("mysql:host=$servername;dbname=emp_mgt_db",$username,$password);
-    $conn = new PDO ("sqlsrv:Server=$servername;Database=emp_mgt_db",$username,$password);
+    $conn = new PDO ("sqlsrv:Server=$servername;Database=emp_mgt_db;TrustServerCertificate=1",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'NO CONNECTION'.$e->getMessage();
