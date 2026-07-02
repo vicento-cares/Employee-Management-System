6df430f1d1be1e4c627ba152472d6b0dd9f65545
vicento-cares <143.careappsoftware@gmail.com>
2026-07-02 10:04:32 +0800

    Get Employee API

---
+++ process/overall_kpi/get_employee.php
@@ -0,0 +1,8 @@
+<?php
+    include '../conn.php';
+    $stmt = $conn ->prepare("EXEC employees_GET_employee :EmpNo");
+    $stmt -> bindValue(":EmpNo",trim($_GET['emp_no']));
+    $stmt -> execute();
+    header('Content-Type: application/json');
+    echo json_encode(["data" => $stmt -> fetch(PDO::FETCH_ASSOC)]);
+    exit();
